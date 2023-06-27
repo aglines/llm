da134c330688b5418d9ae22a61fbbaf89c6f40e3
Andrew Glines <aglines@gmail.com>
2023-06-27 21:54:27 +0200

    scrape text

---
+++ scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https//en.wikipedia.oreg/wiki/GPT-4"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+content_civ = soup.find('div', {'class': 'mw-parser-output'})
+
+unwanted_tags = ['sup','span','table','ul','ol']
+for tag in unwanted_tags:
+    for match in content_civ.findAll(tag):
+        match.extract()
+
+print(content_div.get_text())
+
+                                
