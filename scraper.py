8909c4870d22f0b288a4ac8aa939443537fe3a8e
Andrew Glines <aglines@gmail.com>
2023-06-28 14:31:42 +0200

    venv, typoes

--- scraper.py
+++ scraper.py
@@ -6,11 +6,11 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-content_civ = soup.find('div', {'class': 'mw-parser-output'})
+content_div = soup.find('div', {'class': 'mw-parser-output'})
 
 unwanted_tags = ['sup','span','table','ul','ol']
 for tag in unwanted_tags:
-    for match in content_civ.findAll(tag):
+    for match in content_div.findAll(tag):
         match.extract()
 
 print(content_div.get_text())
